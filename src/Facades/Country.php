d96fc04a2a108866d1d59aadb9dc0eef9c8f9d7b
Eugene <swallow1991@gmail.com>
2025-04-05 16:50:40 +0300

    docs block for facade

--- src/Facades/Country.php
+++ src/Facades/Country.php
@@ -4,6 +4,20 @@ namespace Eugene\CountryTools\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Country
+ *
+ * @method static \Eugene\CountryTools\Services\CountryService getFacadeRoot()
+ * @method static \Illuminate\Support\Collection getCountryCodes()
+ * @method static \Illuminate\Support\Collection getAllCountries()
+ * @method static \Eugene\CountryTools\ValueObjects\Country getCountry(string $code)
+ * @method static string|null getName(string $code)
+ * @method static string|null getFlag(string $code)
+ * @method static string|null getPhoneCode(string $code)
+ * @method static string|null getCurrency(string $code)
+ *
+ * @package Eugene\CountryTools\Facades
+ */
 class Country extends Facade
 {
     protected static function getFacadeAccessor(): string
